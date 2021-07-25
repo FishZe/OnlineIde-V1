c051f6689556abc7f5bdb55329015a84d2a7d4fd
YuFengZe <65108397+YuFengZe@users.noreply.github.com>
2021-07-26 01:29:20 +0800

    Update views.py
    上传错了。。

--- run/views.py
+++ run/views.py
@@ -1,29 +1,31 @@
+import json
+
+from urllib import parse
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.template.context_processors import csrf
 
+from . import run
 from . import ini
 
-
 # Create your views here.
-
-def Index(request):
-    if not ini.OpenWeb:
-        return HttpResponse("404!")
-       
-    request.encoding = 'utf-8'
-    if 'language' in request.GET and request.GET['language']:
-        Lang = request.GET['language']
-        print(Lang)
-    else:
-        Lang = "cpp"
-    
-    Select = SelectDict = {"cpp" : "", "c" : "", "python2" : "", "python3" : "", "java" : "", "go" : ""}
-    Select[Lang] = "Selected"
-    
-    return render(request, "Index.html", {"Url" : ini.Url, "Language" : ini.AceSetting[Lang], "Select" : Select, "csrf_token" : csrf(request)['csrf_token']})
-    
 @csrf_exempt
-def Login(request):
-    return render(request, "Login.html", {"Url" : ini.Url, "csrf_token" : csrf(request)['csrf_token'] })
+def Judge(request):
+    if not ini.OpenJudge:
+        return HttpResponse("404!")
+
+    Data = json.loads(request.body)
+    
+    if Data["password"] != ini.JudgeServersPassword:
+        return HttpResponse("404!")
+    
+    Result = run.PostCompile(ini.JudgesUrl, 
+                             ini.XJudgeServerTokens,
+                             parse.unquote(Data['Code']), 
+                             Data['Time'], 
+                             Data['Memory'], 
+                             Data['Language'], 
+                             ini.TestCaseRode, 
+                             Data['Case'])
+
+    return HttpResponse(json.dumps(Result))  
