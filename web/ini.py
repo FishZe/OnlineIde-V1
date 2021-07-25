9d57b20ae7635687c721240a6ba606ed5de6f256
YuFengZe <65108397+YuFengZe@users.noreply.github.com>
2021-07-26 00:28:16 +0800

    Create ini.py

---
+++ web/ini.py
@@ -0,0 +1,14 @@
+#全局变量 请自行更改设置
+
+OpenWeb = True
+#是否开启web服务
+
+Url = "https://ide.yufengze.org/"
+#网址
+
+AceSetting = {"c" : "c_cpp", "cpp" : "c_cpp", 
+              "java" : "java", "python2" : "python", 
+              "python3" : "python", "go" : "golang"
+            }
+#编辑器相关变量
+
